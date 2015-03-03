c58c3dd990676263ab104c533b92f30a1f550661
gavellaneda <avellaneda.gabriel@gmail.com>
2015-03-03 19:45:34 -0300

    Configured the master server to get values from the modbus driver. Added an initial version of a client project. Exported a Variable from the master server.

---
+++ com.antares.scada.demo.client/src/com/antares/scada/demo/client/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package com.antares.scada.demo.client;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "com.antares.scada.demo.client.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
